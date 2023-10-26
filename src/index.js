3a4994f78dd44d0f00550aa1068bb2c01c12038a
Adam Dziomdziora <adam.dziomdziora@eficode.com>
2023-10-26 14:20:39 +0200

    fixes

--- src/index.js
+++ src/index.js
@@ -1,6 +1,25 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
+import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+if ('serviceWorker' in navigator) {
+    // Register a service worker hosted at the root of the
+    // site using the default scope.
+    navigator.serviceWorker.register("/public/service-worker.js").then(
+        registration => {
+            console.log('Service worker registration succeeded:', registration);
+        },
+      /*catch*/ error => {
+            console.error(`Service worker registration failed: ${error}`);
+        }
+    );
+} else {
+    console.error('Service workers are not supported.');
+}
+
+const root = ReactDOM.createRoot(document.getElementById('root')); root.render(
+    <React.StrictMode>
+        <App />
+    </React.StrictMode>
+);
