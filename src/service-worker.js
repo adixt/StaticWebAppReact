205d2f0be9e942f4b8c14463fd08d9eafd22d729
Adam Dziomdziora <adam.dziomdziora@eficode.com>
2023-10-26 14:35:49 +0200

    fix3

--- src/service-worker.js
+++ src/service-worker.js
@@ -1,1 +1,14 @@
+/* eslint-disable no-restricted-globals */import { clientsClaim } from 'workbox-core';
+import { precacheAndRoute } from 'workbox-precaching';
+clientsClaim();
+
+precacheAndRoute(self.__WB_MANIFEST);
+
 console.log("A");
+
+self.addEventListener('message', (event) => {
+    console.error("A");
+    if (event.data && event.data.type === 'SKIP_WAITING') {
+        self.skipWaiting();
+    }
+});
